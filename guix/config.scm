4d71d89cdbe186cac260b6a6bd167ff3000d50c5
0xhenrique <allyson2308h@gmail.com>
2026-04-23 23:02:56 +0100

    update guix config for 1.5.0

--- guix/config.scm
+++ guix/config.scm
@@ -1,127 +1,87 @@
- ;; This is an operating system configuration generated
-;; by the graphical installer.
-;;
-;; Once installation is complete, you can learn and modify
-;; this file to tweak the system configuration, and pass it
-;; to the 'guix system reconfigure' command to effect your
-;; changes.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   ____   _   _   ___  __  __ ;;
+;;  / ___| | | | | |_ _| \ \/ / ;;
+;; | |  _  | | | |  | |   \  /  ;;
+;; | |_| | | |_| |  | |   /  \  ;;
+;;  \____|  \___/  |___| /_/\_\ ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(add-to-load-path "/home/arisu/.config/guix")
 
-;; Indicate which modules to import to access the variables
-;; used in this configuration.
 (use-modules (gnu)
              (gnu packages)
-             (gnu services samba)
-             (nongnu packages linux))
+             (gnu services sddm)
+             (packages)
+             (services)
+             (filesystems)
+             (nongnu packages linux)
+             (nongnu packages nvidia)
+             (nongnu system linux-initrd)
+             (nonguix transformations))
 
-(use-service-modules cups desktop networking ssh xorg)
+(use-service-modules desktop networking ssh xorg)
 
-(operating-system
-  (locale "en_GB.utf8")
-  (timezone "Europe/Lisbon")
-  (keyboard-layout (keyboard-layout "us"))
-  (host-name "home")
+(define %arisu-os
+  (operating-system
+    (locale "en_GB.utf8")
+    (timezone "Europe/Lisbon")
+    (keyboard-layout (keyboard-layout "us"))
+    (host-name "wired")
 
-  ;; Non-free kernel because of my fucking graphics card
-  (kernel linux)
-  (firmware (list linux-firmware))
+    ;; Blacklist nouveau to avoid conflicts with nvidia driver
+    (kernel-arguments '("modprobe.blacklist=nouveau"))
 
-  ;; The list of user accounts ('root' is implicit).
-  (users (cons* (user-account
-                  (name "arisu")
-                  (comment "arisu")
-                  (group "users")
-                  (home-directory "/home/arisu")
-                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
-                %base-user-accounts))
+    ;; Non-free kernel - pinned for stability with NVIDIA drivers
+    (kernel linux-6.18)
+    (initrd microcode-initrd)
+    (firmware (cons* linux-firmware %base-firmware))
 
-  ;; Packages installed system-wide.  Users can also install packages
-  ;; under their own account: use 'guix search KEYWORD' to search
-  ;; for packages and 'guix install PACKAGE' to install a package.
-  (packages (append (list
-					 (specification->package "nss-certs")
-					 (specification->package "emacs")
-					 (specification->package "emacs-desktop-environment")
-					 (specification->package "emacs-exwm"))
-                    %base-packages))
+    ;; User accounts
+    (users (cons* (user-account
+                   (name "arisu")
+                   (comment "arisu")
+                   (group "users")
+                   (home-directory "/home/arisu")
+                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                  %base-user-accounts))
 
-  ;; Below is the list of system services.  To search for available
-  ;; services, run 'guix system search KEYWORD' in a terminal.
-  (services
-   (append (list (service xfce-desktop-service-type)
-                 (service openssh-service-type)
-                 (service cups-service-type)
-                 (set-xorg-configuration
-                  (xorg-configuration (keyboard-layout keyboard-layout)))
-                 (service samba-service-type 
-                          (samba-configuration
-                           (enable-smbd? #t)
-                           (config-file 
-                            (plain-file "smb.conf" "
-[global]
-# Security settings
-map to guest = Bad User
-logging = syslog@1
-server string = hack me
-workgroup = WORKGROUP
+    ;; System packages
+    (packages (append arisu-desktop-packages
+                      arisu-development-packages
+                      arisu-server-packages
+                      %base-packages))
 
-# Performance
-socket options = TCP_NODELAY IPTOS_LOWDELAY SO_RCVBUF=131072 SO_SNDBUF=131072
+    ;; Services
+    (services (cons*
+               ;; SDDM display manager (GDM has known issues with NVIDIA)
+               (service sddm-service-type)
 
-[hdd1]
-comment = Primary Drive
-browsable = yes
-path = /public/hdd1
-read only = no
-guest ok = yes
-guest only = yes
-create mask = 0644
-directory mask = 0755
+               ;; Server services
+               arisu-samba-service
+               arisu-postgresql-service
 
-[hdd2]
-comment = Secondary Drive  
-browsable = yes
-path = /public/hdd2
-read only = no
-guest ok = yes
-guest only = yes
-create mask = 0644
-directory mask = 0755
-")))))
+               ;; System services
+               (service openssh-service-type)
 
-           ;; This is the default list of services we
-           ;; are appending to.
-           %desktop-services))
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets (list "/boot/efi"))
-                (keyboard-layout keyboard-layout)))
-  (swap-devices (list (swap-space
-                        (target (uuid
-                                 "e7127ac8-58b4-42a3-9aa8-2f4efed0bec0")))))
+               ;; Base desktop services without GDM
+               (modify-services %desktop-services
+                 (delete gdm-service-type))))
 
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
-  (file-systems (cons* (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "418C-8A00"
-                                       'fat32))
-                         (type "vfat"))
-                       
-                       (file-system
-                         (mount-point "/")
-                         (device (uuid
-                                  "9b2a0c03-8a39-49a9-8fc1-94d9a692c4cf"
-                                  'ext4))
-                         (type "ext4"))
+    ;; Boot configuration
+    (bootloader (bootloader-configuration
+                 (bootloader grub-efi-bootloader)
+                 (targets (list "/boot/efi"))
+                 (keyboard-layout keyboard-layout)))
 
-                       (file-system
-                        (mount-point "/public/hdd1") 
-                        (device (uuid "bf91bd86-c9ea-4675-95a8-cc172afdec29" 'ext4))
-                        (type "ext4")
-                        (flags '(no-atime))
-                        (needed-for-boot? #f)
-                        (skip-check-if-clean? #t)
-                        (create-mount-point? #t))
-                       %base-file-systems)))
+    ;; Swap space
+    (swap-devices (list (swap-space
+                         (target (uuid "b47f38e0-3f1f-43be-b255-e708127ceccd")))))
+
+    ;; File systems
+    (file-systems arisu-file-systems)))
+
+;; Apply NVIDIA transformation - handles driver, kernel modules, and Xorg setup
+((nonguix-transformation-nvidia
+  #:driver nvda-beta
+  #:configure-xorg? sddm-service-type)
+ %arisu-os)
